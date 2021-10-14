cbe6fef84f36808c08aed9d68cc6207e25c66e60
ASMB <you@example.com>
2021-10-14 04:19:39 -0500

    Added function and if examples

--- 2021-10-13/ejemplo.cpp
+++ 2021-10-13/ejemplo.cpp
@@ -1,16 +1,30 @@
-// Calcular los números primos entre 1 y 100
+// Calcular los números pares entre 1 y 100
 
 // Incluir las librerías que vamos a usar
 #include <iostream>
 #include <cmath>
 
+void print_even(int nmin, int nmax); // declaración
+
 // main function
 int main(void)
 {
-  int m = 1;
-  int n = 100;
-  std::cout << "m: " << m << "\n";
-  std::cout << "n: " << n << "\n";
-  
+  const int m = 1;
+  const int n = 15;
+
+  print_even(m,n);
+  print_even(m/2, 2*n);
+
   return 0;
+}
+
+void print_even(int nmin, int nmax) // implementación
+{  
+  for(int ii = nmin; ii <= nmax ; ii = ii+1){
+    //si el número es par, entonces imprimir
+    if (ii%2 ==  0) {
+      std::cout << ii << " ";
+    }
+  }
+  std::cout << "\n"; 
 }
