58f5f3844e31efc9feafe5a7cce011dd6e94a00e
ASMB <you@example.com>
2021-10-14 03:20:55 -0500

    Primer ejemplo

---
+++ 2021-10-13/ejemplo.cpp
@@ -0,0 +1,16 @@
+// Calcular los números primos entre 1 y 100
+
+// Incluir las librerías que vamos a usar
+#include <iostream>
+#include <cmath>
+
+// main function
+int main(void)
+{
+  int m = 1;
+  int n = 100;
+  std::cout << "m: " << m << "\n";
+  std::cout << "n: " << n << "\n";
+  
+  return 0;
+}
