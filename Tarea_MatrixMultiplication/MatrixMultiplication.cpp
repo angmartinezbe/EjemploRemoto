9508476e92d972e534c72d02e378498002d52aaa
ASMB <you@example.com>
2021-11-30 04:39:08 -0500

    Added a loop to print all N values

--- Tarea_MatrixMultiplication/MatrixMultiplication.cpp
+++ Tarea_MatrixMultiplication/MatrixMultiplication.cpp
@@ -13,23 +13,27 @@ int main(int argc, char **argv) {
   const int N = std::atoi(argv[1]);
   const int SEED = std::atoi(argv[2]);
 
-  std::vector<double> A(N*N, 0.0), B(N*N, 0.0), C(N*N, 0.0);
+  for (int ii = 2; ii <= 9; ++ii){
 
-  std::mt19937 gen(SEED);
-  std::uniform_real_distribution<> dist(0.,1.);
+    int N = pow(2, ii);
+
+    std::vector<double> A(N*N, 0.0), B(N*N, 0.0), C(N*N, 0.0);
+
+    std::mt19937 gen(SEED);
+    std::uniform_real_distribution<> dist(0.,1.);
  
-  std::generate(A.begin(), A.end(), [&gen, &dist](){ return dist(gen); });   
-  std::generate(B.begin(), B.end(), [&gen, &dist](){ return dist(gen); });
-
-  auto start = std::chrono::high_resolution_clock::now();
-  multiply(A, B, C);
-  auto stop = std::chrono::high_resolution_clock::now();
+    std::generate(A.begin(), A.end(), [&gen, &dist](){ return dist(gen); });   
+    std::generate(B.begin(), B.end(), [&gen, &dist](){ return dist(gen); });
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    multiply(A, B, C);
+    auto stop = std::chrono::high_resolution_clock::now();
+    
+    std::cout << N << "\t";
+    auto elapsed = std::chrono::duration<double>(stop - start);
+    std::cout << elapsed.count()/1.401e-06 << "\n"; //1.401e-06 : Tiempo cuando N=4
+  }
   
-  std::cout << C[N/2] << std::endl;
-  
-  auto elapsed = std::chrono::duration<double>(stop - start);
-  std::cout << elapsed.count()/1.401e-06 << "\n"; //1.401e-06 : Tiempo cuando N=4
-
   return 0;
 }
 
