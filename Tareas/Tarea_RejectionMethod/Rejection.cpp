d707e168cd7a4a9b1fb68fa1114148c65aef5059
ASMB <you@example.com>
2022-02-01 07:23:49 -0500

    Solution

--- Tareas/Tarea_RejectionMethod/Rejection.cpp
+++ Tareas/Tarea_RejectionMethod/Rejection.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
     std::mt19937 gen(SEED);
     std::uniform_real_distribution<double> xu(-1.0, 1.0);
     std::uniform_real_distribution<double> yu(0.0, 3.0/4.0);
-    std::ofstream fout("data.txt");
+    std::ofstream fout("datos.txt");
     for (int ii = 0; ii < SAMPLES; ++ii) {
         double xr = xu(gen);
         double yr = yu(gen);
